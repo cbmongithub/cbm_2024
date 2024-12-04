e2d3ce59c949f8ad6f3a32b9a57a3e3b5143c9fc
Christian Martinez <hello@christianbmartinez.com>
2024-12-04 13:58:10 -0700

    og

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,23 +1,20 @@
 import { ImageResponse } from 'next/og'
 
-export async function GET(req: Request, res: Response) {
-  const url = new URL(req.url)
-  const title = url.searchParams.get('title') || 'Christian B. Martinez | Full stack developer'
+export function GET(request: Request) {
+	let url = new URL(request.url);
+	let title = url.searchParams.get("title") || "Next.js Portfolio Starter";
 
-  return await new ImageResponse(
-    (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
-            {title}
-          </h2>
-        </div>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  )
-
+	return new ImageResponse(
+		<div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+			<div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+				<h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
+					{title}
+				</h2>
+			</div>
+		</div>,
+		{
+			width: 1200,
+			height: 630,
+		},
+	);
 }
