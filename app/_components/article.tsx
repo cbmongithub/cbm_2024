87a097bab40403220e5295509e2cd142a16f4c61
Christian Martinez <hello@christianbmartinez.com>
2024-12-03 20:11:12 -0700

    Stroke width, line join, changes and href changes to only activate on link click.

--- app/_components/article.tsx
+++ app/_components/article.tsx
@@ -17,29 +17,24 @@ export function Article({
 }) {
 	return (
 		<article className="rounded-2xl group relative flex flex-col items-start">
-			<h2 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-				<Link
-					href={href}
-					className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
-				/>
-				<a href="/articles/crafting-a-design-system-for-a-multiplanetary-future">
-					<span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
-					<span className="relative z-10">{title}</span>
-				</a>
+			<h2 className="text-base font-semibold tracking-tight text-neutral-800 dark:text-neutral-100">
+				<div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-neutral-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-neutral-800/50 sm:-inset-x-6 sm:rounded-2xl" />
+				<span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
+				<span className="relative z-10">{title}</span>
 			</h2>
 			<time
-				className="relative z-10 order-first mb-3 flex items-center pl-3.5 text-sm text-zinc-400 dark:text-zinc-500"
+				className="relative z-10 order-first mb-3 flex items-center pl-3.5 text-sm text-neutral-400 dark:text-neutral-500"
 				dateTime={dateTime}
 			>
 				<span
 					className="absolute inset-y-0 left-0 flex items-center"
 					aria-hidden="true"
 				>
-					<span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+					<span className="h-4 w-0.5 rounded-full bg-neutral-200 dark:bg-neutral-500" />
 				</span>
 				{date}
 			</time>
-			<p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+			<p className="relative z-10 mt-2 text-sm text-neutral-600 dark:text-neutral-400">
 				{description}
 			</p>
 			<Link
@@ -56,9 +51,9 @@ export function Article({
 				>
 					<path
 						d="M6.75 5.75 9.25 8l-2.5 2.25"
-						stroke-width="1.5"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeWidth="1.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					/>
 				</svg>
 			</Link>
