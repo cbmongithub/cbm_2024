db03f5b75a7df905b7f744922c0e16ad60577e82
Christian Martinez <hello@christianbmartinez.com>
2024-12-05 20:44:38 -0700

    404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,22 +1,44 @@
+import Image from "next/image";
 import Link from "next/link";
+import { Header } from "./_components/header";
 
 export default function NotFound() {
   return (
-			<div className="flex h-full items-center pt-16 sm:pt-32">
-				<div className="flex flex-col items-center">
-					<p className="text-base font-semibold text-neutral-400 dark:text-neutral-500">
-						404
-					</p>
-					<h1 className="mt-4 text-4xl font-bold tracking-tight text-neutral-800 sm:text-5xl dark:text-neutral-100">
-						Page not found
-					</h1>
+			<>
+				<Header title="404" description="Page not found" />
+				<div className="flex flex-col justify-center items-center h-[45vh]">
+					<Image
+						className="rounded-2xl mt-16"
+						src="https://media1.tenor.com/m/lx2WSGRk8bcAAAAC/pulp-fiction-john-travolta.gif"
+						alt="John Travolta confused"
+						width={300}
+						height={300}
+					/>
 					<p className="mt-4 text-base text-neutral-600 dark:text-neutral-400">
 						The page you have requested does not exist
 					</p>
-					<Link href="/" className="mt-4">
-						Go home
+					<Link
+						href="/"
+						aria-hidden="true"
+						className="relative z-20 mt-4 flex items-center text-sm font-medium text-blue-500 hover:text-blue-600"
+					>
+						Go to home
+						<svg
+							viewBox="0 0 16 16"
+							fill="none"
+							aria-hidden="true"
+							className="ml-1 h-4 w-4 stroke-current"
+						>
+							<path
+								d="M6.75 5.75 9.25 8l-2.5 2.25"
+								strokeWidth="1.5"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+						</svg>
 					</Link>
 				</div>
-			</div>
+			</>
 		);
+
 }
