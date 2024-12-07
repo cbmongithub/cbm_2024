98d26995cbc7904ffae449e885944d1c1116c32c
Christian Martinez <hello@christianbmartinez.com>
2024-12-07 00:59:12 -0700

    container

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/_components/header";
 import { CardAlt } from "@/_components/ui/card-alt";
+import { Container } from "@/_components/ui/container";
 import { formatDate, mapper } from "@/_lib/helpers";
 import { getPosts } from "@/_lib/posts";
 
@@ -15,29 +16,23 @@ export default function Page() {
 	return (
 		<>
 			<Header title="Portfolio" description="Some of my favorite projects" />
-			<div className="w-full">
-				<div className="relative">
-					<div className="flex flex-col">
-						<h1 className="mt-32 text-2xl font-semibold tracking-tight text-neutral-800 dark:text-neutral-100">
-							Projects
-						</h1>
-						<div className="pb-10">
-							{mapper(recentProjects, (post) => (
-								<CardAlt
-									key={post.slug}
-									content="project"
-									contentUrl={`/portfolio/${post.slug}`}
-									title={post.metadata.title}
-									date={formatDate(post.metadata.publishedAt)}
-									description={post.metadata.summary}
-									imgSrc={post.metadata.image as string}
-									imgAlt={post.metadata.imageAlt as string}
-								/>
-							))}
-						</div>
-					</div>
-				</div>
-			</div>
+			<Container>
+				<h1 className="mt-32 text-2xl font-semibold tracking-tight text-neutral-800 dark:text-neutral-100">
+					Projects
+				</h1>
+				{mapper(recentProjects, (post) => (
+					<CardAlt
+						key={post.slug}
+						content="project"
+						contentUrl={`/portfolio/${post.slug}`}
+						title={post.metadata.title}
+						date={formatDate(post.metadata.publishedAt)}
+						description={post.metadata.summary}
+						imgSrc={post.metadata.image as string}
+						imgAlt={post.metadata.imageAlt as string}
+					/>
+				))}
+			</Container>
 		</>
 	);
 }
