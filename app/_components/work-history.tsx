a304dd2c189d283f4357124d7ccdcf18e8c3724d
Christian Martinez <hello@christianbmartinez.com>
2024-12-10 12:07:12 -0700

    download

--- app/_components/work-history.tsx
+++ app/_components/work-history.tsx
@@ -111,7 +111,6 @@ export function WorkHistory() {
     <Link
       className="group mt-6 inline-flex w-full items-center justify-center rounded-md bg-neutral-50 px-3 py-2 text-sm font-medium text-neutral-900 outline-offset-2 transition hover:bg-neutral-100 active:bg-neutral-100 active:text-neutral-900/75 active:transition-none dark:bg-neutral-800/75 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 dark:active:bg-neutral-800/50 dark:active:text-neutral-50/70"
       href="/"
-      download
     >
       Download CV
       <svg
