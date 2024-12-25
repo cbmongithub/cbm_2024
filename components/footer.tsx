c848150aa36e54fcb15b5ca0028b2ff1abcad400
Christian Martinez <hello@christianbmartinez.com>
2024-12-25 09:57:13 -0700

    color ratio

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,27 +4,33 @@ import { links } from "lib/config";
 
 export const Footer = () => {
   return (
-    <footer className="relative bottom-0 left-0 flex-none pt-32" aria-label="Footer">
-      <div className="mx-auto w-full max-w-7xl lg:px-8">
-        <div className="border-t pb-16 pt-10 border-neutral-700/40">
-          <div className="relative px-4 sm:px-8 lg:px-12">
-            <div className="mx-auto max-w-2xl lg:max-w-5xl">
-              <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+    <footer
+      className='relative bottom-0 left-0 flex-none pt-32'
+      aria-label='Footer'
+    >
+      <div className='mx-auto w-full max-w-7xl lg:px-8'>
+        <div className='border-t pb-16 pt-10 border-neutral-700/40'>
+          <div className='relative px-4 sm:px-8 lg:px-12'>
+            <div className='mx-auto max-w-2xl lg:max-w-5xl'>
+              <div className='flex flex-col items-center justify-between gap-6 sm:flex-row'>
                 <ul
-                  className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-neutral-200"
-                  aria-label="Footer Links"
+                  className='flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-neutral-200'
+                  aria-label='Footer Links'
                 >
                   {links.footer.map(({ href, text }) => {
                     return (
                       <li key={text}>
-                        <Link className="transition hover:text-blue-600" href={href}>
+                        <Link
+                          className='transition hover:text-blue-600'
+                          href={href}
+                        >
                           {text}
                         </Link>
                       </li>
-                    );
+                    )
                   })}
                 </ul>
-                <p className="text-sm text-neutral-500">
+                <p className='text-sm text-neutral-400'>
                   &copy;&nbsp;Christian B. Martinez {new Date().getFullYear()}
                 </p>
               </div>
@@ -33,5 +39,5 @@ export const Footer = () => {
         </div>
       </div>
     </footer>
-  );
+  )
 }
