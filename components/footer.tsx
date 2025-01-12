cd2d335c91c4b88bca29d8e4e3c6da021ae8cbfe
Christian Martinez <hello@christianbmartinez.com>
2025-01-12 12:42:01 -0700

    Border

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
       aria-label='Footer'
     >
       <div className='mx-auto w-full max-w-7xl lg:px-8'>
-        <div className='border-t pb-16 pt-10 border-neutral-700/40'>
+        <div className='border-t pb-16 pt-10 border-neutral-800'>
           <div className='relative px-4 sm:px-8 lg:px-12'>
             <div className='mx-auto max-w-2xl lg:max-w-5xl'>
               <div className='flex flex-col items-center justify-between gap-6 sm:flex-row'>
